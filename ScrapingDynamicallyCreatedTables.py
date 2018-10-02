ad9e9fd1f0b3b711568c643db0beef569bf009f2
sarfraz <m.sarfraz.arshad@gmail.com>
2018-10-02 12:15:31 +0500

    get html of element with that contains table from the webdriver

--- ScrapingDynamicallyCreatedTables.py
+++ ScrapingDynamicallyCreatedTables.py
@@ -6,6 +6,8 @@ Created on Tue Oct  2 10:12:11 2018
 @author: sarfraz
 """
 from selenium import webdriver
+import pandas as pd
+import sys
 
 #URL from where the tables will be scraped
 URL= 'http://www.scstrade.com/stockscreening/SS_CompanySnapShotYR.aspx?symbol=FATIMA'
@@ -13,7 +15,16 @@ URL= 'http://www.scstrade.com/stockscreening/SS_CompanySnapShotYR.aspx?symbol=FA
 #define driver as firefox webdriver 
 driver = webdriver.Firefox()
 
-#make get request for the url that opens the page in firefox
+#loads the page in firefox
 driver.get(URL)
 
+#get the html element at a specific xpath
+element = driver.find_element_by_xpath('//form[1]')
 
+#extract the html from that element
+element_html=element.get_attribute('innerHTML')
+print(element_html);
+
+#close the browser / webdriver
+driver.close()
+    
